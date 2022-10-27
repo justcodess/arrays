984e9fc8d62283fbd98068d1492c3447dbd3a2ae
aidata <aidata@10.121.3.186>
2022-10-27 10:44:59 +0300

    array2

--- arrays/src/arrays/array.java
+++ arrays/src/arrays/array.java
@@ -3,11 +3,25 @@ package arrays;
 public class array {
 
 	public static void main(String[] args) {
-		int[]array=new int[10];
+		int[]array= {32,27,64,18,95,14,90,70,60,37};
 		System.out.printf("%s%8s%n", "Index","Value");
 		
-		for(int counter=0; counter<array.length;counter++) {
-			System.out.printf("%5d%8d%n",counter,array[counter] );
+		int a=9;
+		for(int counter=0; counter<array.length/2;counter++) {
+		
+			int temp = array[counter];
+			array[counter]=array[a];
+			temp=array[a];
+			a--;
+		}
+			
+			for(int counter=0;counter<array.length;counter++) {
+				System.out.printf("%5d%8d%n",counter,array[counter] );
+				
+			}
+			
+			
+			
 			
 			
 			
@@ -16,4 +30,4 @@ public class array {
 
 	}
 
-}
+
